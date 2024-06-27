7dc16177f70754ace7163a2caf25fcfa7ff0ac30
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2024-06-27 17:57:05 +0600

    feat: add mailpit

---
+++ cli/Valet/Mailpit.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Valet;
+
+class Mailpit
+{
+    public function __construct(
+        public Brew $brew,
+        public Site $site,
+        public Configuration $config
+    ){}
+
+    public function install(): void
+    {
+        if (!$this->brew->installed('mailpit')) {
+            $this->brew->installOrFail('mailpit');
+            $this->brew->restartService('mailpit');
+            $tld = $this->config->read()['tld'];
+
+            $this->site->proxyCreate(
+                url: "mailpit.{$tld}",
+                host: 'http://localhost:8025',
+                secure: true
+            );
+        }
+    }
+
+    public function stop(): void
+    {
+        $this->brew->stopService('mailpit');
+    }
+
+    public function uninstall(): void
+    {
+        $this->brew->uninstallFormula('mailpit');
+    }
+}
